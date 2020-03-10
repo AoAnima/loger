be0d8376c459e9d55becee08534bd3bd66df595b
AO <34207320+AoAnima@users.noreply.github.com>
2020-03-10 22:21:47 +0300

    Update util.go

--- util.go
+++ util.go
@@ -2,29 +2,35 @@ package loger
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 // Описание цветов ANSII
 //http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
-func инфо(формат string,  данные ...interface{}) {
-
+var StdLog = log.New(os.Stderr, "", log.Llongfile)
+//var Stdout = log.New(os.Stdout, "", log.Llongfile)
+func Инфо(формат string,  данные ...interface{}) {
+	StdLog.SetFlags(log.Lshortfile|log.Ltime)
 	формат = strings.ReplaceAll(формат, "%+v", "\u001b[38;5;48m %+v  \u001b[0m\u001b[38;5;75m")
 
-	str := fmt.Sprintf("\u001b[0m \u001b[36m ИНФО: \u001b[38;5;75m " + формат +" \u001b[0m \n", данные...)
-	_, _ = os.Stdout.WriteString(str)
-	//if err != nil {
-	//	log.Printf("err %+v\n", err)
-	//}
+	str := fmt.Sprintf("\u001b[0m\u001b[36mИНФО: \u001b[38;5;75m " + формат +" \u001b[0m \n", данные...)
+	err := StdLog.Output(2, str)
+
+	if err != nil {
+		log.Printf("%+v", err)
+	}
+
 }
-func ошибка(формат string,  данные ...interface{}) {
+
+func Ошибка(формат string,  данные ...interface{}) {
+	StdLog.SetFlags(log.Lshortfile|log.Ltime)
 	формат = strings.ReplaceAll(формат, "%+v", "\u001b[38;5;204m %+v  \u001b[0m\u001b[38;5;1m")
 
-	str := fmt.Sprintf("\u001b[48;5;124m ОШИБКА: \u001b[0m  \u001b[38;5;1m  " + формат +" \u001b[0m \n", данные...)
+	str := fmt.Sprintf("\u001b[48;5;124m ОШИБКА >> \u001b[0m  \u001b[38;5;1m  " + формат +" \u001b[0m \n", данные...)
 
-	_, _ = os.Stdout.WriteString(str)
-	//if err != nil {
-	//	log.Printf("err %+v\n", err)
-	//}
+	err := StdLog.Output(2, str)
+	if err != nil {
+		log.Printf("%+v", err)
+	}
 }
-
